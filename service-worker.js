ab96416c5d992e4b823373167aecbb1b8873e25d
Fordeath22 <alexandre.petibon@yahoo.fr>
2025-11-02 11:52:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const APP_FILES_TO_CACHE = [
   'script.js',
   'manifest.json',
   'icon-512.png',
-  'dragon-bg-light.png' // NOUVEAU FOND
+  'dragon-bg-light.jpg' // NOUVEAU FOND
 ];
 
 // --- Événement d'installation ---
@@ -49,3 +49,4 @@ self.addEventListener('activate', (event) => {
   );
 
 });
+
